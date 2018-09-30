60f1093281e094d108fb13438e34ace9b27e5692
Alan Cecil <alan.cecil@gmail.com>
2018-09-29 22:51:49 -0700

    aliens!

--- alanryancecil.py
+++ alanryancecil.py
@@ -2,11 +2,15 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route('/')
 def hello():
     return render_template('index.html')
 
+
+@app.route('/aliens')
+def aliens():
+    return render_template('aliens.html')
+
 if __name__ == "__main__":
-    print('yes this code ran on the server')
-    # app.run()
-    app.run(host='0.0.0.0')
+    app.run()
+    # app.run(host='0.0.0.0')
